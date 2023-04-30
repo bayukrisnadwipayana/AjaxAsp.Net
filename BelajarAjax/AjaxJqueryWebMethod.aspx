9d3db284526a81bb645c08af2e116cb8b71179e1
bayukrisnadwipayana <85788956+bayukrisnadwipayana@users.noreply.github.com>
2023-04-30 15:00:11 +0800

    Add files via upload

---
+++ BelajarAjax/AjaxJqueryWebMethod.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AjaxJqueryWebMethod.aspx.cs" Inherits="BelajarAjax.AjaxJqueryWebMethod" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+   
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <label for="id">ID Provinsi</label>
+    <br />
+    <asp:TextBox ID="idprovinsi" runat="server"></asp:TextBox>
+    <br />
+    <label for="nama">Nama Provinsi</label>
+    <br />
+    <asp:TextBox ID="namaprovinsi" runat="server"></asp:TextBox>
+    <br />
+    <input type="button" id="btnsave" value="Save" onclick="SaveProvinsi()" />
+    </div>
+    </form>
+     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script type="text/javascript">
+            function SaveProvinsi()
+            {
+                var param={id:$('#<%= idprovinsi.ClientID %>').val(),
+                            nama:$('#<%= namaprovinsi.ClientID %>').val()
+                };
+                $.ajax({
+                    type:'POST',
+                    url:'AjaxJqueryWebMethod.aspx/SaveProvinsi',
+                    data:JSON.stringify(param),
+                    contentType:'application/Json;charset:utf-8',
+                    dataType:'Json',
+                    success:function(data){},
+                    error:function(){}
+                });
+                alert('Data Sukses Disimpan');
+            }
+        </script>
+</body>
+</html>
+
---
+++ BelajarAjax/AjaxJqueryWebMethod.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+namespace BelajarAjax
+{
+    public partial class AjaxJqueryWebMethod : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void DisplayDataBase()
+        {
+
+        }
+        [WebMethod]
+        public static void SaveProvinsi(string id, string nama)
+        {
+            SqlConnection koneksi = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\USER\Documents\latihan.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            string query = "INSERT INTO provinsi VALUES(@id,@nama)";
+            SqlCommand command = new SqlCommand();
+            try
+            {
+                koneksi.Open();
+                command.Connection = koneksi;
+                command.CommandType = CommandType.Text;
+                command.CommandText = query;
+                command.Parameters.Add("@id", SqlDbType.VarChar).Value = id;
+                command.Parameters.Add("@nama", SqlDbType.VarChar).Value = nama;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+            finally
+            {
+                koneksi.Close();
+            }
+        }
+    }
+}
